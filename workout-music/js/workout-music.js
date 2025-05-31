a2099f1509e9901c9e15dcd7287b50b1efdd282f
ninja-k <leeshinglo@gmail.com>
2025-05-31 17:16:01 +0800

    Refactor: Move style presets to separate file and restore hip hop preset

--- workout-music/js/workout-music.js
+++ workout-music/js/workout-music.js
@@ -1,4 +1,6 @@
 // Main WorkoutMusic class with speech synthesis support
+import { stylePresets } from './workout-music-presets.js';
+
 export class WorkoutMusic {
     constructor() {
         // Prevent multiple instances
@@ -25,121 +27,7 @@ export class WorkoutMusic {
         this.voiceCues = true;
 
         // Style presets
-        this.stylePresets = {
-            edm: {
-                name: 'EDM',
-                instruments: ['kick', 'snare', 'hihat', 'bass', 'chords', 'lead'],
-                pattern: [
-                    // Kick, Snare, HiHat, Bass, Chords, Lead
-                    [1, 0, 1, 1, 1, 0], [0, 0, 1, 0, 0, 0], [0, 0, 1, 1, 0, 0], [1, 0, 1, 0, 0, 1],
-                    [1, 1, 1, 0, 0, 0], [0, 0, 1, 1, 0, 0], [1, 0, 1, 0, 1, 0], [0, 1, 1, 1, 0, 0]
-                ],
-                chordProgressions: [
-                    // I - V - vi - IV (EDM classic)
-                    [
-                        ['C3', 'E3', 'G3'], ['G3', 'B3', 'D4'],
-                        ['A3', 'C4', 'E4'], ['F3', 'A3', 'C4']
-                    ],
-                    // vi - IV - I - V (Emotional)
-                    [
-                        ['A3', 'C4', 'E4'], ['F3', 'A3', 'C4'],
-                        ['C3', 'E3', 'G3'], ['G3', 'B3', 'D4']
-                    ]
-                ],
-                bassNotes: ['C2', 'G2', 'A2', 'F2'],
-                leadPattern: [
-                    ['C4', 'D4', 'E4', 'F4'],
-                    ['E4', 'F4', 'G4', 'A4'],
-                    ['G4', 'A4', 'B4', 'C5'],
-                    ['A4', 'G4', 'F4', 'E4']
-                ],
-                baseFrequency: 110,
-                bpmRange: { min: 120, max: 150 },
-                intensityMultiplier: 1.6,
-                swing: 0.2,
-                effects: {
-                    reverb: 0.3,
-                    delay: 0.2,
-                    distortion: 0.1
-                }
-            },
-            rock: {
-                name: 'Rock',
-                instruments: ['kick', 'snare', 'hihat', 'guitar', 'bass', 'chords'],
-                pattern: [
-                    // Kick, Snare, HiHat, Guitar, Bass, Chords
-                    [1, 0, 0, 1, 1, 0], [0, 0, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 1, 1, 0, 1],
-                    [1, 0, 0, 0, 1, 0], [0, 0, 1, 0, 0, 0], [1, 1, 0, 1, 0, 1], [0, 0, 1, 0, 0, 0],
-                    [1, 0, 0, 1, 1, 0], [0, 0, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 1, 1, 0, 1],
-                    [1, 0, 0, 0, 1, 0], [0, 0, 1, 0, 0, 0], [1, 1, 0, 1, 0, 1], [0, 0, 1, 0, 1, 0]
-                ],
-                chordProgressions: [
-                    // I - V - vi - IV (Rock classic)
-                    [
-                        ['E3', 'G#3', 'B3'], ['B3', 'D#4', 'F#4'],
-                        ['C#4', 'E4', 'G#4'], ['A3', 'C#4', 'E4']
-                    ],
-                    // I - IV - V - IV (Rock and Roll)
-                    [
-                        ['E3', 'G#3', 'B3'], ['A3', 'C#4', 'E4'],
-                        ['B3', 'D#4', 'F#4'], ['A3', 'C#4', 'E4']
-                    ]
-                ],
-                bassNotes: ['E2', 'B2', 'C#3', 'A2'],
-                leadPattern: [
-                    ['E4', 'F#4', 'G#4', 'A4'],
-                    ['G#4', 'A4', 'B4', 'C#5'],
-                    ['B4', 'C#5', 'D#5', 'E5'],
-                    ['A4', 'G#4', 'F#4', 'E4']
-                ],
-                baseFrequency: 150,
-                bpmRange: { min: 90, max: 120 },
-                intensityMultiplier: 1.2,
-                swing: 0.1,
-                effects: {
-                    reverb: 0.4,
-                    delay: 0.1,
-                    distortion: 0.6
-                }
-            },
-            hiphop: {
-                name: 'Hip Hop',
-                instruments: ['kick', 'snare', 'hihat', 'bass', 'chords', 'fx'],
-                pattern: [
-                    // Kick, Snare, HiHat, Bass, Chords, FX
-                    [1, 0, 1, 1, 1, 0], [0, 0, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 0, 1, 0, 0, 1],
-                    [0, 0, 1, 1, 0, 0], [1, 0, 1, 0, 0, 0], [0, 1, 0, 0, 1, 0], [0, 0, 1, 0, 0, 1],
-                    [1, 0, 1, 1, 1, 0], [0, 0, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 0, 1, 0, 0, 1],
-                    [0, 0, 1, 1, 0, 0], [1, 0, 1, 0, 0, 0], [0, 1, 0, 0, 1, 0], [0, 0, 1, 0, 0, 1]
-                ],
-                chordProgressions: [
-                    // i - VII - v - VI (Hip Hop minor)
-                    [
-                        ['F#3', 'A3', 'C#4'], ['E3', 'G#3', 'B3'],
-                        ['C#3', 'E3', 'G#3'], ['D#3', 'F#3', 'A#3']
-                    ],
-                    // i - iv - v - VII (Trap progression)
-                    [
-                        ['F#3', 'A3', 'C#4'], ['B3', 'D#4', 'F#4'],
-                        ['C#4', 'E4', 'G#4'], ['E3', 'G#3', 'B3']
-                    ]
-                ],
-                bassNotes: ['F#1', 'E1', 'C#2', 'D#2'],
-                fxPattern: [
-                    'vinyl_scratch', 'none', 'none', 'snare_roll',
-                    'none', 'none', 'riser', 'none'
-                ],
-                baseFrequency: 75,
-                bpmRange: { min: 75, max: 105 },
-                intensityMultiplier: 0.9,
-                swing: 0.3,
-                effects: {
-                    reverb: 0.5,
-                    delay: 0.4,
-                    bitCrusher: 4
-                }
-            }
-        };
+        this.stylePresets = stylePresets;
 
         // Audio nodes and state
         this.instruments = {};
